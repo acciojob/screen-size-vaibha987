3700183afb98ecf7465f82e1e20ac842b935b63d
vaibha987 <v2goyal95@gmail.com>
2024-08-30 22:15:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const heading=document.getElementsById("sizeInfo").querySelector("h1")
+function updateSize(){
+heading.innexrText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+}
+updateSize()
+// Update the size whenever the window is resized
+window.addEventListener('resize', updateSize);
